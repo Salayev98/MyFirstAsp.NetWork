576460ada9b5d60639e80e014a97004f5032aec6
Your NameAnar <you@example.com>
2021-12-22 01:11:47 +0400

    My first Asp.Net Practice

---
+++ MyFirstASP.NetProject/MyFirstASP.NetProject/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstASP.NetProject.Controllers
+{
+    public class HomeController:Controller
+    {
+        //public ContentResult Index(int id)
+        //{
+        //    var result = new ContentResult();
+        //    result.Content = "Salam Anar Salayev" + "Menim Yawim " + id.ToString() + " -dir";
+        //    return result;
+        //}
+        public ActionResult Json()
+        {
+            var result = new JsonResult(new { name="anar",surname= "salayev"});
+            return result;
+        }
+        public ActionResult Detail(int id, string surname, string name)
+        {
+            ViewData["Id"] = id;
+            ViewData["Surname"] = surname;
+            ViewData["Name"] = name;
+            ViewBag.Id = id;
+            ViewBag.Surname= surname;
+            ViewBag.Name = name;
+            TempData["Id"] = id;
+            TempData["Surname"] = surname;
+            TempData["Name"] = name;
+
+            return View();
+        }
+        public ActionResult Index()
+        {
+            ViewResult view = new ViewResult();
+            view.ViewName = "Index";
+            return view;
+        }
+        public ActionResult About()
+        {
+            ViewResult view = new ViewResult();
+            view.ViewName = "About";
+            return view;
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
